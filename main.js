7bc4f38bc75bd21157724a1c1ff50e6cb7c26cb2
benn-christensen <138111085+benn-christensen@users.noreply.github.com>
2023-10-25 15:37:23 +0000

    added animate funktion

--- main.js
+++ main.js
@@ -12,3 +12,8 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
